e5a0b0ddb2ef21101acc16fd74165ebf3a0321bd
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-31 19:13:56 -0400

    chore: check for duplicate deps when `yarn.lock` changes

--- .eslintrc.base.cjs
+++ .eslintrc.base.cjs
@@ -118,6 +118,7 @@ module.exports = {
           'commonjs',
           'cts',
           'ctx',
+          'dedupe',
           'dotenv',
           'enum',
           'esm',
--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -7,15 +7,20 @@ const { extensions } = require('./.vscode/settings.json')['eslint.options']
 
 module.exports = {
   /**
-   * Formatting command rules.
-   */
-  '*': ['yarn fix:format', 'git add -A'],
-
-  /**
    * Linting command rules.
    */
   [`*.{${extensions.map(ext => ext.slice(1)).join()}}`]: [
     'yarn fix:style',
     'git add -A'
-  ]
+  ],
+
+  /**
+   * Formatting command rules.
+   */
+  '*': ['yarn fix:format', 'git add -A'],
+
+  /**
+   * Local integrity check.
+   */
+  'yarn.lock': ['yarn check:dedupe']
 }
